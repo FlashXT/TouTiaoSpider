b2037128c69ff7a7ec86d836a5051735ec8f078e
FlashXT <SXXT2015@outlook.com>
2018-07-10 08:02:14 +0800

    Version3.0,update log: 将图片下载保存到本地，按每个图集的名称保存到Image文件夹中；

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+#MongoDB配置文件
 MONGO_URL ='localhost'
 MONGO_DB='toutiaojiepai'
 MONGO_TABLE='toutiaoImage'
--- get_image.py
+++ get_image.py
@@ -1,9 +1,8 @@
 #coding = utf-8
 #####################################################################
-#简介：分析Ajax抓取今日头条街拍美图,Version2.0(爬取一个offset的多个图集的每
-#     个图集的多个图片的url.)
+#简介：分析Ajax抓取今日头条街拍美图,Version3.0
 #Author:FlashXT
-#Date:2018/7/9,Monday,rainy
+#Date:2018/7/8,Monday,rainy
 #Copyright © 2018–2020 FlashXT and turboMan. All rights reserved.
 #####################################################################
 import re
@@ -43,7 +42,7 @@ def get_gallery_url(offset,keyword):
         return "Request Error!"
 
 def parse_gallery_url(text):
-    '''通过response内容解析出图集的真实地址'''
+    '''通过response内容解析出图集的真实地址并转换'''
     try:
         data = json.loads(text)
         if data and 'data' in data.keys():
@@ -56,7 +55,8 @@ def parse_gallery_url(text):
     except JSONDecodeError:
         print("JSON Decode Error!")
 
-def get_image_url(url):
+def get_image_html(url):
+    '''获取图集url的responsen内容'''
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -66,6 +66,7 @@ def get_image_url(url):
         return None
 
 def parse_image_url(url,html):
+    '''从图集的response内容中提取图集标题，照片地址'''
     if html:
         soup = BeautifulSoup(html,'lxml')
         result = soup.select('title')
@@ -86,29 +87,62 @@ def parse_image_url(url,html):
     else:return None
 
 def save_to_mongo(dic):
+    #将
     if db[MONGO_TABLE].insert(dic):
-        return '存储到MONGO_DB:Successful!'
+        print('存储到MONGO_DB Successful！')
+        return True
     return False
 
+
+def download_image(result):
+    for item in result["image_url"]:
+
+        try:
+            response = requests.get(item)
+            if response.status_code == 200:
+                print("Downloading ", item, "...",save_image(result['title'],response.content))
+
+        except RequestException:
+            print("请求图片出错", item)
+            return "Request Error!"
+
+def save_image(title,content):
+    try :
+        file_dic = '{0}/{1}'.format(os.getcwd(),str("\Image\\"+title))
+        # print(file_dic)
+        file_path ='{0}/{1}.{2}'.format(os.getcwd()+str("\Image\\"+title),md5(content).hexdigest(),'jpg')
+        # print(file_path)
+        if not os.path.exists(file_dic):
+            os.makedirs(file_dic)
+        with open(file_path,'wb') as img:
+            img.write(content)
+            img.close()
+        return True
+    except Exception :
+	    # print("Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        return False
 def main():
     for offset in range(6):
-        print("offset--->", offset*20)
+        print("offset--->", offset * 20)
         #获取每一个offset的多个图集地址
         text= get_gallery_url(offset*20,"街拍")
         #对每个图集的地址进行转化
         parsed_url = parse_gallery_url(text)
         #获取每个图集中的照片地址
         for item in parsed_url:
-            html = get_image_url(item)
+            html = get_image_html(item)
             try:
-               result = parse_image_url(item,html)
-               if result:
-                   if result['image_url'] is not None:
-                       print(item,save_to_mongo(result))
-               else:return None
-            except:continue
+                result = parse_image_url(item,html)
+                if result:
+                    if result['image_url'] is not None:
+                       #下载语句
+                        download_image(result)
+                       #存入MongoDB的语句
+                       # print(item)
+                       # save_to_mongo(result)
 
+            except: continue
 
 if __name__ == "__main__":
-     main()
+    main()
 
